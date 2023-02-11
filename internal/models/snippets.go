61b4f8933a304fdc53ad1a63b77312458c32ee4d
TaskMasterErnest <ernestklu08@gmail.com>
2023-02-11 16:36:32 +0000

    added a standalone errors file to handle errors concerning models

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -41,7 +42,25 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 
 // return snippet based on its id
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
-	return nil, nil
+
+	statement := `SELECT id, content, created, expires FROM snippets
+	WHERE expires > UTC_TIMESTAMP() AND id=?`
+
+	row := m.DB.QueryRow(statement, id)
+
+	s := &Snippet{}
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	return s, nil
 }
 
 // return 10 most recently created snippets
