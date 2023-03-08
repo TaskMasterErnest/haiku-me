f6f14df59e1fcb95b3b6024a36c46059db1172f1
TaskMasterErnest <ernestklu08@gmail.com>
2023-03-08 15:40:30 +0000

    modified routes using the julienschmidt/httprouter

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,16 +12,12 @@ func (app *application) routes() http.Handler {
 	// prepping a server
 	router := httprouter.New()
 
-	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		app.notFound(w)
-	})
-
 	//serving the static files for frontend
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
 	router.HandlerFunc(http.MethodGet, "/", app.home)
-	router.HandlerFunc(http.MethodGet, "/snippet/view", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
 	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
 	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
