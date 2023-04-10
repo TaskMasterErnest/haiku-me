23a3537137161feb7a6fae3e95a04e67ebaf7f93
TaskMasterErnest <ernestklu08@gmail.com>
2023-04-10 09:06:01 +0000

    added a table driven test

--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -6,13 +6,40 @@ import (
 )
 
 func TestHumanDate(t *testing.T) {
-	//initialize a new time.Time object and pass it to the humandate function
-	tm := time.Date(2022, 4, 10, 10, 15, 0, 0, time.UTC)
-	hd := humanDate(tm)
+	//an anonymous struct containing the test case name,
+	//input to the humanDate (tm field) and the expected output (the want field)
+	tests := []struct {
+		name string
+		tm   time.Time
+		want string
+	}{
+		{
+			name: "UTC",
+			tm:   time.Date(2023, 4, 10, 10, 15, 0, 0, time.UTC),
+			want: "10 Apr 2023 at 10:15",
+		},
+		{
+			name: "Empty",
+			tm:   time.Time{},
+			want: "",
+		},
+		{
+			name: "CET",
+			tm:   time.Date(2023, 4, 10, 10, 15, 0, 0, time.FixedZone("CET", 1*60*60)),
+			want: "10 Apr 2023 at 09:15",
+		},
+	}
 
-	//check that the output of humandat is what is expected
-	//if not use the t.Errorf() function to indicate the test has failed and log the expected and actual results
-	if hd != "10 Apr 2022 at 10:15" {
-		t.Errorf("got %q; want %q", hd, "10 Apr 2022 at 10:15")
+	//Loop over the test cases
+	for _, tt := range tests {
+		//the t.Run() is used to run a sub-test for each test case.
+		//first param is the name used to identify the test, second param is the anon func containing the actual test
+		t.Run(tt.name, func(t *testing.T) {
+			hd := humanDate(tt.tm)
+
+			if hd != tt.want {
+				t.Errorf("got %q; want %q", hd, tt.want)
+			}
+		})
 	}
 }
